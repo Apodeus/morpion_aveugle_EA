a72ef1633278de227a16d7aa56dce65e33d1763e
Apodeus <ordonez.romain@gmail.com>
2017-01-06 16:57:01 +0100

    end fusion

--- main_reseau.py
+++ main_reseau.py
@@ -22,62 +22,68 @@ play_mode = 0;
 expect_answer = 0;
 
 class grid:
-    cells = []
-    def __init__(self):
-        self.cells = []
-        for i in range(NB_CELLS):
-            self.cells.append(EMPTY)
+		cells = []
+		def __init__(self):
+				self.cells = []
+				for i in range(NB_CELLS):
+						self.cells.append(EMPTY)
 
-    def play(self, player, cellNum):
-        assert(0<= cellNum and cellNum < NB_CELLS)
-        assert(self.cells[cellNum] == EMPTY)
-        self.cells[cellNum] = player
+		def play(self, player, cellNum):
+				assert(0<= cellNum and cellNum < NB_CELLS)
+				assert(self.cells[cellNum] == EMPTY)
+				self.cells[cellNum] = player
 
-    """ Display the state of the game
-        Example of output : 
-        -------
-        |O| |X|
-        -------
-        |X|O| |
-        -------
-        | | |O| 
-        -------
-    """
-    def display(self):
-        print("-------------")
-        for i in range(3):
-            print("|",symbols[self.cells[i*3]], "|",  symbols[self.cells[i*3+1]], "|",  symbols[self.cells[i*3+2]], "|");
-            print("-------------")
+		""" Display the state of the game
+				Example of output : 
+				-------
+				|O| |X|
+				-------
+				|X|O| |
+				-------
+				| | |O| 
+				-------
+		"""
+		def display(self):
+				print("-------------")
+				for i in range(3):
+						print("|",symbols[self.cells[i*3]], "|",  symbols[self.cells[i*3+1]], "|",  symbols[self.cells[i*3+2]], "|");
+						print("-------------")
+						
+		def displayStr(self):
+				grid_str = "$display $"
+				for i in range(3):
+						grid_str = grid_str + str(self.cells[i*3]) +  str(self.cells[i*3+1]) + str(self.cells[i*3+2])
+				return grid_str
 
-    """ Test if 'player' wins the game"""
-    def winner(self, player):
-        assert(player==J1 or player==J2)
-        # horizontal line
-        for y in range(3): 
-            if self.cells[y*3] == player and self.cells[y*3+1] == player and self.cells[y*3+2] == player:
-                    return True
-        # vertical line
-        for x in range(3): 
-            if self.cells[x] == player and self.cells[3+x] == player and self.cells[6+x] == player:
-                    return True
-        #diagonals :
-        if self.cells[0] == player and self.cells[4] == player and self.cells[8] == player:
-            return True
-        if self.cells[2] == player and self.cells[4] == player and self.cells[6] == player:
-            return True
-        return False
-    
-    """ Return the state of the game: -1 if the game is not over, EMPTY if DRAW; J1 if player 1 wins and J2 if player 2 wins.
-    """
-    def gameOver(self):
-        if self.winner(J1):
-            return J1
-        if self.winner(J2):
-            return J2
-        for i in range(NB_CELLS):
-            if(self.cells[i] == EMPTY):
-                return -1
-        return 0
+		""" Test if 'player' wins the game"""
+		def winner(self, player):
+				assert(player==J1 or player==J2)
+				# horizontal line
+				for y in range(3): 
+						if self.cells[y*3] == player and self.cells[y*3+1] == player and self.cells[y*3+2] == player:
+										return True
+				# vertical line
+				for x in range(3): 
+						if self.cells[x] == player and self.cells[3+x] == player and self.cells[6+x] == player:
+										return True
+				#diagonals :
+				if self.cells[0] == player and self.cells[4] == player and self.cells[8] == player:
+						return True
+				if self.cells[2] == player and self.cells[4] == player and self.cells[6] == player:
+						return True
+				return False
+		
+		""" Return the state of the game: -1 if the game is not over, EMPTY if DRAW; J1 if player 1 wins and J2 if player 2 wins.
+		"""
+		def gameOver(self):
+				if self.winner(J1):
+						return J1
+				if self.winner(J2):
+						return J2
+				for i in range(NB_CELLS):
+						if(self.cells[i] == EMPTY):
+								return -1
+				return 0
 
 
 class Client:
